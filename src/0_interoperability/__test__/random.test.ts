05f5e7113e3c3190acea3d8e4df48076ca221fe0
alstn2468 <alstn2468_@naver.com>
2021-04-20 22:05:12 +0900

    random 함수 테스트 작성

--- src/0_interoperability/__test__/random.test.ts
+++ src/0_interoperability/__test__/random.test.ts
@@ -1,1 +1,10 @@
 import { random } from '../random';
+
+describe('random함수 테스트 (랜덤)', () => {
+  let result;
+  it('Math.random함수를 mocking해 테스트하기', () => {
+    Math.random = jest.fn().mockReturnValue(0.5);
+    result = random();
+    expect(result).toBe(0.5);
+  });
+});
