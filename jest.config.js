d4fb0b28da9feb40ccd642cbf2571ce2e46eb8c1
alstn2468 <alstn2468_@naver.com>
2021-04-20 22:28:00 +0900

    pretty 명령어 추가, .prettierignore 추가

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,10 @@
-
 module.exports = {
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   transform: {
-    "^.+\\.tsx?$": "ts-jest"
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node",
-  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
   coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx,js,jsx}',
-    '!src/**/*.d.ts',
-  ],
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.d.ts'],
 };
--- src/0_interoperability/__test__/readFileSync.test.ts
+++ src/0_interoperability/__test__/readFileSync.test.ts
@@ -1,1 +1,3 @@
 import { readFileSync } from '../readFileSync';
+
+describe('readFileSync함수 테스트 (동기 부수 효과)', () => {});
