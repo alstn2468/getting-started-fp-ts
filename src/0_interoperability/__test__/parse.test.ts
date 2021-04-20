ea1724e4c14fab901383d1d20bd4bad8b3d80a0e
alstn2468 <alstn2468_@naver.com>
2021-04-20 21:44:28 +0900

    parse함수 테스트 코드 작성

--- src/0_interoperability/__test__/parse.test.ts
+++ src/0_interoperability/__test__/parse.test.ts
@@ -1,1 +1,24 @@
 import { parse } from '../parse';
+import { isLeft, isRight, getOrElse } from 'fp-ts/Either';
+
+describe('parse함수 테스트 (예외)', () => {
+  const success = '{"a": 1, "b": 2}';
+  const fail = '{"a": 1, "b"}';
+  let result;
+  it('parse함수가 정상적으로 실행됐을 경우', () => {
+    result = parse(success);
+    expect(isRight(result)).toBeTruthy();
+    expect(getOrElse(() => ({ error: true }))(result)).toMatchObject({
+      a: 1,
+      b: 2,
+    });
+  });
+
+  it('parse함수 실행 중 예외가 발생했을 경우', () => {
+    result = parse(fail);
+    expect(isLeft(result)).toBeTruthy();
+    expect(getOrElse(() => ({ a: 1 }))(result)).toMatchObject({
+      a: 1,
+    });
+  });
+});
