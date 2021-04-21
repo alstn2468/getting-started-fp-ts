5276e33947e390c50e1dc37138b07f8581fc86bd
alstn2468 <alstn2468_@naver.com>
2021-04-21 22:44:30 +0900

    eqNumber 테스트 설명 수정

--- src/1_eq/__test__/eqNumber.test.ts
+++ src/1_eq/__test__/eqNumber.test.ts
@@ -1,6 +1,6 @@
 import { eqNumber } from '../eqNumber';
 
-describe('eqNumber 함수 테스트', () => {
+describe('Eq 인터페이스를 구현한 eqNumber 객체 테스트', () => {
   it('eqNumber 객체 equals 메서드 테스트', () => {
     expect(eqNumber.equals(1, 1)).toBeTruthy();
     expect(eqNumber.equals(1, 2)).toBeFalsy();
