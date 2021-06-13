211d8fe24bb4afdac43c9d15a1c62ab48e5d7a30
alstn2468 <alstn2468_@naver.com>
2021-06-13 12:23:54 +0900

    fix: Date 객체 Mocking이 정상적으로 되지 않는 것 같아 테스트

--- src/getting_started_series/10_io/__test__/now.test.ts
+++ src/getting_started_series/10_io/__test__/now.test.ts
@@ -1,9 +1,15 @@
 import { now } from '../now';
 
 describe('실패하지 않는 연산을 표현하는 추상화 IO를 사용한 now함수 테스트', () => {
+  beforeEach(() => {
+    jest
+      .useFakeTimers('modern')
+      .setSystemTime(new Date(2021, 6, 13, 0, 0, 0, 0));
+  });
+  afterEach(() => {
+    jest.useRealTimers();
+  });
   it('getTime 함수가 mocking된 값을 정상적으로 반환하는지 테스트', () => {
-    jest.useFakeTimers('modern');
-    jest.setSystemTime(new Date(2021, 6, 13, 0, 0, 0, 0));
     expect(now()).toBe(1626102000000);
   });
 });
