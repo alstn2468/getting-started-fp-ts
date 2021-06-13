c763e696688a6c7191418a778beaed3747aee114
alstn2468 <alstn2468_@naver.com>
2021-06-13 12:39:33 +0900

    fix: 타임존을 UTC로 변경, now 함수 테스트 코드 수정

--- src/getting_started_series/10_io/__test__/now.test.ts
+++ src/getting_started_series/10_io/__test__/now.test.ts
@@ -10,6 +10,6 @@ describe('실패하지 않는 연산을 표현하는 추상화 IO를 사용한 n
     jest.useRealTimers();
   });
   it('getTime 함수가 mocking된 값을 정상적으로 반환하는지 테스트', () => {
-    expect(now()).toBe(1626102000000);
+    expect(now()).toBe(1626134400000);
   });
 });
