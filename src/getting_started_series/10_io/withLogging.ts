af9944954d0e780d2176841664d25bfc31949a46
alstn2468 <alstn2468_@naver.com>
2021-06-19 17:51:58 +0900

    chore: withLogging 함수 원문에 있는 주석도 추가

--- src/getting_started_series/10_io/withLogging.ts
+++ src/getting_started_series/10_io/withLogging.ts
@@ -2,5 +2,6 @@ import type { IO } from 'fp-ts/lib/IO';
 import { chain, map } from 'fp-ts/lib/IO';
 import { log } from 'fp-ts/lib/Console';
 
+/** 디버깅을 위해 콘솔에 값을 기록한다. */
 export const withLogging = <A>(action: IO<A>): IO<A> =>
   chain<A, A>((a) => map(() => a)(log(`Value is: ${a}`)))(action);
