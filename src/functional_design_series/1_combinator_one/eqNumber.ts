05c713ec6dffc295cb9819c69b5b97a9056b34b6
alstn2468 <alstn2468_@naver.com>
2021-07-12 15:36:31 +0900

    chore: fix miss typo

--- src/functional_design_series/1_combinator_one/eqNumber.ts
+++ src/functional_design_series/1_combinator_one/eqNumber.ts
@@ -1,6 +1,6 @@
 import type { Eq } from 'fp-ts/lib/Eq';
 
-/** `number`을 위한 원시적인 `Eq` 인스턴스 */
+/** `number`를 위한 원시적인 `Eq` 인스턴스 */
 export const eqNumber: Eq<number> = {
   equals: (x, y) => x === y,
 };
