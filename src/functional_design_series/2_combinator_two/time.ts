7b234d320f63ddf227b6bd6ebb23a3fd8bd8c2c1
Minsu Kim <alstn2468_@naver.com>
2021-07-20 19:51:07 +0900

    fix: chain, map 함수를 Monad에서 가져와 쓰도록 개선

--- src/functional_design_series/2_combinator_two/time.ts
+++ src/functional_design_series/2_combinator_two/time.ts
@@ -1,21 +1,9 @@
 import type { IO } from 'fp-ts/lib/IO';
 import { now } from 'fp-ts/lib/Date';
-import { pipe } from 'fp-ts/lib/function';
-import { chain, map } from 'fp-ts/lib/IO';
+import { Monad } from 'fp-ts/lib/IO';
 
 export function time<A>(ma: IO<A>): IO<[A, number]> {
-  return pipe(
-    now,
-    chain((start) =>
-      pipe(
-        ma,
-        chain((a: A) =>
-          pipe(
-            now,
-            map((end) => [a, end - start]),
-          ),
-        ),
-      ),
-    ),
+  return Monad.chain(now, (start) =>
+    Monad.chain(ma, (a) => Monad.map(now, (end) => [a, end - start])),
   );
 }
